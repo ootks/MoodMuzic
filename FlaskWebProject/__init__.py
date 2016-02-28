cc57cf2ed022b62d9e0f975d8525e42078992885
Kevin Shu <shu.kevins@gmail.com>
2016-02-28 09:14:46 -0800

    Added songs file

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -35,14 +35,15 @@ def getEmotion():
 
 @app.route('/getSongs', methods = ['POST'])
 def getSong():
-    if 'image' not in request.form:
-        return "fuck"
-    image = request.form['image']
-    image = re.sub(".*base64,", "", image)
-    image = base64.b64decode(image)
-    emots = emotions.getEmotions(image)
-    print >> sys.stderr, str(emots)
-    print >> sys.stderr, str(type(emots))
-    stoof = str(songs.getSongs(emots))
-    print >> sys.stderr, stoof
-    return stoof
+	return "music"
+    # if 'image' not in request.form:
+    #     return "fuck"
+    # image = request.form['image']
+    # image = re.sub(".*base64,", "", image)
+    # image = base64.b64decode(image)
+    # emots = emotions.getEmotions(image)
+    # print >> sys.stderr, str(emots)
+    # print >> sys.stderr, str(type(emots))
+    # stoof = str(songs.getSongs(emots))
+    # print >> sys.stderr, stoof
+    # return stoof
