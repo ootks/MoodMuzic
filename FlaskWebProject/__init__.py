a580749f96a6edfa0fa355d153f72c59ba24cb14
Kevin Shu <shu.kevins@gmail.com>
2016-02-28 00:38:05 -0800

    Set up Flask

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -5,4 +5,8 @@ The flask application package.
 from flask import Flask
 app = Flask(__name__)
 
-import FlaskWebProject.views
+#import FlaskWebProject.views
+
+@app.route('/')
+def hello_world():
+    return "Hello World!"
